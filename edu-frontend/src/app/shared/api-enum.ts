9df557c2e6c3ffdd4a7fbc333e9310e211529a56
Rajasekhara018 <rajshekarbadam001@gmail.com>
2025-07-31 22:20:33 +0530

    Login Api Integration

--- edu-frontend/src/app/shared/api-enum.ts
+++ edu-frontend/src/app/shared/api-enum.ts
@@ -1,5 +1,6 @@
 export enum APIPath {
-
+    AUTH_REGISTER ="/api/auth/register",
+    AUTH_LOGIN ="/api/auth/login",
     ROLE_GETALL = "/api/v1/uam/role/getAll",
     USER_GETALL = "/api/v1/uam/user/getAll",
     USER_AUTHENTICATE = "/api/v1/uam/otp/authenticate",
