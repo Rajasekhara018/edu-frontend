5ac22f852c589df2a114ce736af943f1d9f7b516
Rajasekhara018 <rajshekarbadam001@gmail.com>
2025-08-04 21:37:00 +0530

    Website setup

---
+++ webSite/src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  isMobileMenuOpen = false;
+
+  toggleMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMobileMenuOpen = false;
+  }
+}
